d8e71fb35df936c4c7d623616f06afb96f89b5ac
ersantoli <ersantoli@gmail.com>
2022-05-18 16:29:44 -0300

    to do challenger finished

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import '../styles/header.scss'
+
+export function Header() {
+  return (
+    <header className="header">
+      <div>
+        <h1 className=''>To.Do</h1>
+      </div>
+    </header>
+  )
+}
